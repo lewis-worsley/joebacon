db8dbece2a33efa698f872805f78d963ced05895
lewis-worsley <lewisworsley4@hotmail.co.uk>
2024-11-07 21:29:24 +0000

    wip: created intial header and nav for site

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,28 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import { Button } from './ui/button'
 
 const Header = () => {
 	return (
-		<header>
-			<nav className='container'>
-				Header
+		<header className='drop-shadow-lg shadow-md sticky'>
+			<nav className='container py-5'>
+				<div className='flex flex-row justify-between'>
+					<div>
+						<Image
+							src="/logo.svg"
+							alt='Joe Bacon logo'
+							width={120}
+							height={20}
+						/>
+					</div>
+					<div className='space-x-10 md:space-x-20 font-bold text-blue-1 text-[14px]'>
+						<Link href="">Services</Link>
+						<Link href="">About</Link>
+						<Link href="">Portfolio</Link>
+						<Button>Free Consultation</Button>
+					</div>
+				</div>
 			</nav>
 		</header>
 	)
