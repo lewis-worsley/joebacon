95a5ee86be040f58d32b3fd8afa0004bc0a4f62c
lewis-worsley <lewisworsley4@hotmail.co.uk>
2024-11-08 14:22:52 +0000

    moved nav into a list to map over

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,31 +1,47 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import { Button } from './ui/button'
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from './ui/button';
 
 const Header = () => {
+	const navLinks = [
+		{
+			name: "Services",
+			href: "#services",
+		},
+		{
+			name: "About",
+			href: "#about",
+		},
+		{
+			name: "Portfolio",
+			href: "#portfolio",
+		},
+	];
+
 	return (
 		<header className='drop-shadow-lg shadow-md sticky'>
 			<nav className='container py-5'>
 				<div className='flex flex-row justify-between'>
 					<div>
-						<Image
-							src="/logo.svg"
-							alt='Joe Bacon logo'
-							width={120}
-							height={20}
-						/>
+						<Link href="/">
+							<Image
+								src="/logo.svg"
+								alt='Joe Bacon logo'
+								width={120}
+								height={20}
+							/>
+						</Link>
 					</div>
 					<div className='space-x-10 md:space-x-20 font-bold text-blue-1 text-[14px]'>
-						<Link href="">Services</Link>
-						<Link href="">About</Link>
-						<Link href="">Portfolio</Link>
+						{navLinks.map(({ name, href }) => (
+							<Link href={href} key={name}>{name}</Link>
+						))}
 						<Button>Free Consultation</Button>
 					</div>
 				</div>
 			</nav>
 		</header>
-	)
-}
+	);
+};
 
 export default Header
