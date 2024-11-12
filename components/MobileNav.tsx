3ae3eb12ca02b2fd3cd932dba3d9f3f177fd0776
lewis-worsley <lewisworsley4@hotmail.co.uk>
2024-11-12 13:23:31 +0000

    fix: fixed errors regarding accessibility in MobileNav

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,46 +1,45 @@
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
 import {
 	Sheet,
 	SheetClose,
 	SheetContent,
-	SheetTrigger
-} from "@/components/ui/sheet"
+	SheetTitle,
+	SheetTrigger,
+} from "@/components/ui/sheet";
 
-import { navLinks } from "@/constants"
-import { Menu } from 'lucide-react'
-import Image from "next/image"
-import Link from "next/link"
+import { navLinks } from "@/constants";
+import { Menu } from 'lucide-react';
+import Image from "next/image";
+import Link from "next/link";
 
 const MobileNav = () => {
 	return (
 		<section className='sm:hidden'>
 			<Sheet>
 				<SheetTrigger asChild>
-					<Menu
-						className="cursor-pointer"
-					/>
+					<Menu className="cursor-pointer" aria-label="menu" />
 				</SheetTrigger>
 				<SheetContent
 					side="top"
 					className='border-none'
 					aria-describedby="Navigation menu; the following links include Services, About and Portfolio"
 				>
+					<SheetTitle className="hidden">Navigation</SheetTitle>
 					<SheetClose asChild>
 						<Link href="/">
 							<Image
 								src="logo.svg"
 								alt="logo"
 								width={120}
-								height={20}
-							/>
+								height={20} />
 						</Link>
 					</SheetClose>
 					<div className="overflow-y-auto">
 						<nav className="flex flex-col gap-y-10 my-10">
 							{navLinks.map(({ label, href }) => (
-								<SheetClose key={label} className="hover:text-blue-2">
+								<SheetClose key={label} className="text-center" asChild>
 									<Link href={href} className="font-bold text-blue-1 hover:text-gray-500">{label}</Link>
 								</SheetClose>
 							))}
