a4ef8661d74e296da2346ea180aa5df70e98e9a2
lewis-worsley <lewisworsley4@hotmail.co.uk>
2024-11-21 22:03:19 +0000

    adds toast to app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
 
 const openSans = localFont({
   src: "./fonts/OpenSans.woff",
@@ -32,6 +33,7 @@ export default function RootLayout({
         className={`${openSans.variable} ${merriweatherSans.variable} antialiased`}
       >
         {children}
+        <Toaster />
       </body>
     </html>
   );
