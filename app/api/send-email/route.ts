3c4781d9043ca89f3cdb08472cbc1dcf5feeebfa
lewis-worsley <lewisworsley4@hotmail.co.uk>
2024-11-21 14:16:18 +0000

    chore: removed Promise and tidied file

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -1,15 +1,15 @@
 import nodemailer from "nodemailer";
 
-export async function POST(req: Request): Promise<Response> {
+export async function POST(req: Request) {
 	try {
 		const body = await req.json();
-		console.log(body)
 		const { email, subject, message } = body;
 
 		if (!email || !subject || !message) {
-			return new Response(JSON.stringify({ error: "All fields are required" }), {
-				status: 400,
-			});
+			return new Response(
+				JSON.stringify({ error: "All fields are required" }),
+				{ status: 400 }
+			);
 		}
 
 		const transporter = nodemailer.createTransport({
@@ -20,8 +20,6 @@ export async function POST(req: Request): Promise<Response> {
 				user: process.env.EMAIL_USER,
 				pass: process.env.EMAIL_PASS,
 			},
-			debug: true, // Enable debugging
-			logger: true, // Log SMTP interactions
 		});
 
 		const mailOptions: {
@@ -34,19 +32,21 @@ export async function POST(req: Request): Promise<Response> {
 			from: "noreply@joebaconconsulting.com",
 			to: process.env.EMAIL_USER as string,
 			replyTo: email,
-			subject: subject,
+			subject,
 			text: `Message from: ${email}\n\n${message}`,
 		};
 
 		await transporter.sendMail(mailOptions);
 
-		return new Response(JSON.stringify({ success: "Email sent successfully" }), {
-			status: 200,
-		});
-	} catch (error) {
+		return new Response(
+			JSON.stringify({ success: "Email sent successfully" }),
+			{ status: 200 }
+		);
+	} catch (error: any) {
 		console.error("Error sending email:", error);
-		return new Response(JSON.stringify({ error: "Failed to send email" }), {
-			status: 500,
-		});
+		return new Response(
+			JSON.stringify({ error: "Failed to send email" }),
+			{ status: 500 }
+		);
 	}
 }
