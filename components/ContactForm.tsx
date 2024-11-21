0271a9f39391219770e618d73305206aabb56081
lewis-worsley <lewisworsley4@hotmail.co.uk>
2024-11-21 17:39:04 +0000

    adjusted form layout and colours to match brand

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,21 +1,22 @@
-"use client"
+"use client";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
 	Form,
 	FormControl,
+	FormDescription,
 	FormField,
 	FormItem,
 	FormLabel,
 	FormMessage
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { useState } from "react"
-import { Textarea } from "./ui/textarea"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { Textarea } from "./ui/textarea";
 
 const ContactFormSchema = z.object({
 	email: z.string().email("Please enter a valid email address"),
@@ -23,10 +24,10 @@ const ContactFormSchema = z.object({
 	message: z.string().min(10, "Message must be at least 10 characters long"),
 });
 
-type ContactFormValues = z.infer<typeof ContactFormSchema>
+type ContactFormValues = z.infer<typeof ContactFormSchema>;
 
 const ContactForm = () => {
-	const [status, setStatus] = useState<string | null>(null)
+	const [status, setStatus] = useState<string | null>(null);
 
 	const form = useForm<ContactFormValues>({
 		resolver: zodResolver(ContactFormSchema),
@@ -35,7 +36,7 @@ const ContactForm = () => {
 			subject: "",
 			message: "",
 		}
-	})
+	});
 
 	const onSubmit = async (data: ContactFormValues) => {
 		try {
@@ -61,54 +62,87 @@ const ContactForm = () => {
 	}
 
 	return (
-		<section className="container py-20">
-			<Form {...form}>
-				<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-1/3">
-					<FormField
-						control={form.control}
-						name="email"
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Email</FormLabel>
-								<FormControl>
-									<Input placeholder="Enter your email" {...field} />
-								</FormControl>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-					<FormField
-						control={form.control}
-						name="subject"
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Subject</FormLabel>
-								<FormControl>
-									<Input placeholder="Enter subject" {...field} />
-								</FormControl>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-					<FormField
-						control={form.control}
-						name="message"
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Message</FormLabel>
-								<FormControl>
-									<Textarea className="h-52" placeholder="Enter your message" {...field} />
-								</FormControl>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-					<Button type="submit">Submit</Button>
-					{status && <p className="mt-2 text-center text-sm text-gray-600">{status}</p>}
-				</form>
-			</Form>
+		<section className="py-20 bg-contact-form">
+			<div className="container grid grid-cols-1 md:grid-cols-3 gap-8">
+				<div className="md:col-span-2 bg-white-1 p-10 rounded-lg shadow-lg text-center md:text-start">
+					<Form {...form}>
+						<form
+							onSubmit={form.handleSubmit(onSubmit)}
+							className="space-y-5 align"
+						>
+							<div className="space-y-3">
+								<h2 className="text-2xl">Let's Work Together</h2>
+								<p>You're welcome to book a free consultation, just mention a date and time in your message.</p>
+							</div>
+
+							<FormField
+								control={form.control}
+								name="email"
+								render={({ field }) => (
+									<FormItem className="grid md:grid-cols-3 items-center">
+										<FormLabel className="col-span-1">Email*</FormLabel>
+										<FormControl className="md:col-span-2">
+											<Input placeholder="youremail@address.com" {...field} />
+										</FormControl>
+										<FormMessage className="col-span-3" />
+									</FormItem>
+								)}
+							/>
+
+							<FormField
+								control={form.control}
+								name="subject"
+								render={({ field }) => (
+									<FormItem className="grid md:grid-cols-3 items-center">
+										<FormLabel className="col-span-1">Subject line*</FormLabel>
+										<FormControl className="md:col-span-2">
+											<Input placeholder="Enter subject line" {...field} />
+										</FormControl>
+										<FormMessage className="col-span-3" />
+									</FormItem>
+								)}
+							/>
+
+							<FormField
+								control={form.control}
+								name="message"
+								render={({ field }) => (
+									<FormItem className="grid md:grid-cols-3 items-start gap-2">
+										<FormLabel className="col-span-1">Message*</FormLabel>
+										<div className=" md:col-span-2 space-y-2">
+											<FormDescription>
+												Tell me about your business and what services you're interested in.
+											</FormDescription>
+											<FormControl>
+												<Textarea
+													className="h-32"
+													placeholder="Tell me how I can help you."
+													{...field}
+												/>
+											</FormControl>
+										</div>
+										<FormMessage className="col-span-3" />
+									</FormItem>
+								)}
+							/>
+
+							<Button type="submit" className="md:float-end px-5">
+								Submit
+							</Button>
+							{status && (
+								<p className="mt-2 text-center text-sm text-gray-600">{status}</p>
+							)}
+						</form>
+					</Form>
+				</div>
+				<div className="space-y-5 md:col-span-1">
+					<h3 className="text-xl p-10 text-center md:text-start">Helpful information</h3>
+
+				</div>
+			</div>
 		</section>
-	)
-}
+	);
+
+};
 
 export default ContactForm;
